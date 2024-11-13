99976dd3ce3d82c9916b977c6bc9d0f866fd3289
amnakamran1 <Amnakamran2024@gmail.com>
2024-11-12 22:20:05 -0800

    Polymorphism

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Polymorphism.car;
+
+namespace Polymorphism
+{
+    public class Transformer
+    {
+        public virtual void Transform()
+        {
+            Console.WriteLine("The transformer is trasforming");
+        }
+    }
+    public class car : Transformer
+    {
+        public override void Transform()
+        {
+            {
+                Console.WriteLine("Robot transform into Car");
+            }
+        }
+        public class Boat:Transformer
+        {
+            public override void Transform()
+            {
+
+                Console.WriteLine("Jet transform into Robot.");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Transformer myTransformer=new Transformer();
+            Transformer mycar = new car();
+            Transformer myboat = new Boat();
+            myTransformer.Transform();
+            mycar.Transform();
+            myboat.Transform();
+            Console.ReadKey();
+        }
+    }
+}
